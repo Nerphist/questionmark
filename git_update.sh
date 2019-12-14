b4e78dac6d50912fa681da88c944a053a23ab557
Nerphist <nikolailgv@ukr.net>
2019-12-14 20:26:28 +0200

    sh for updating added

---
+++ git_update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]] ; then
+    echo 'Branch not defined. Usage: ./git_update.sh master'
+    exit 1
+fi
+
+git fetch --all
+git reset --hard origin/$1
+
+source venv/bin/activate
+
+python3 manage.py runserver 10.0.1.129:8888 --noreload
+
+chmod 0775 ./git_update.sh
