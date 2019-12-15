0761da14b56547c3a8ccfd1bf2607d870eef1be1
Nerphist <nikolailgv@ukr.net>
2019-12-15 09:40:41 +0200

    get_all_methods method improved

--- questionmark/views.py
+++ questionmark/views.py
@@ -7,12 +7,16 @@ from rest_framework.response import Response
 @api_view(['GET'])
 def get_all_methods(request, **kwargs):
     from .urls import urlpatterns
+
+    def remove_signs(url_info):
+        return str(url_info).replace('^', '').replace('$', '')
+
     urls = []
     for url_obj in urlpatterns:
         if isinstance(url_obj, URLResolver):
             for url in url_obj.url_patterns:
-                urls.append({str(url.name): str(url_obj.pattern) + str(url.pattern)})
+                urls.append({str(url.name): remove_signs(url_obj.pattern) + remove_signs(url.pattern)})
         else:
             url_line = url_obj.pattern
-            urls.append({url_line.name: str(url_line)})
+            urls.append({url_line.name: remove_signs(url_line).replace('^', '')})
     return Response(data=urls, status=status.HTTP_200_OK)
